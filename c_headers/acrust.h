45306b38c11fd2ba080677d8796cec7651cc7ec5
zdivelbiss <8019570+zdivelbiss@users.noreply.github.com>
2025-06-05 15:18:12 -0500

    fix missing definitions

--- c_headers/acrust.h
+++ c_headers/acrust.h
@@ -49,6 +49,17 @@
 #define ACPI_USE_DO_WHILE_0
 #define ACPI_IGNORE_PACKAGE_RESOLUTION_ERRORS
 
+// #ifndef ACPI_DISASSEMBLER
+// #define ACPI_DISASSEMBLER
+// #endif
+
+// #ifndef ACPI_DEBUGGER
+// #define ACPI_DEBUGGER
+// #endif
+
+
+#ifdef __KERNEL__
+
 #define ACPI_USE_SYSTEM_INTTYPES
 #define ACPI_USE_GPE_POLLING
 
@@ -71,14 +82,14 @@
 /* Use a specific bugging default separate from ACPICA */
 
 #undef ACPI_DEBUG_DEFAULT
-#define ACPI_DEBUG_DEFAULT (ACPI_LV_INFO | ACPI_LV_REPAIR)
+#define ACPI_DEBUG_DEFAULT          (ACPI_LV_INFO | ACPI_LV_REPAIR)
 
 #ifndef CONFIG_ACPI
 
 /* External globals for __KERNEL__, stubs is needed */
 
-#define ACPI_GLOBAL(t, a)
-#define ACPI_INIT_GLOBAL(t, a, b)
+#define ACPI_GLOBAL(t,a)
+#define ACPI_INIT_GLOBAL(t,a,b)
 
 /* Generating stubs for configurable ACPICA macros */
 
@@ -92,33 +103,33 @@
 /* External interface for __KERNEL__, stub is needed */
 
 #define ACPI_EXTERNAL_RETURN_STATUS(Prototype) \
-    static ACPI_INLINE Prototype { return (AE_NOT_CONFIGURED); }
+    static ACPI_INLINE Prototype {return(AE_NOT_CONFIGURED);}
 #define ACPI_EXTERNAL_RETURN_OK(Prototype) \
-    static ACPI_INLINE Prototype { return (AE_OK); }
+    static ACPI_INLINE Prototype {return(AE_OK);}
 #define ACPI_EXTERNAL_RETURN_VOID(Prototype) \
-    static ACPI_INLINE Prototype { return; }
+    static ACPI_INLINE Prototype {return;}
 #define ACPI_EXTERNAL_RETURN_UINT32(Prototype) \
-    static ACPI_INLINE Prototype { return (0); }
+    static ACPI_INLINE Prototype {return(0);}
 #define ACPI_EXTERNAL_RETURN_PTR(Prototype) \
-    static ACPI_INLINE Prototype { return (NULL); }
+    static ACPI_INLINE Prototype {return(NULL);}
 
 #endif /* CONFIG_ACPI */
 
 /* Host-dependent types and defines for in-kernel ACPICA */
 
-#define ACPI_MACHINE_WIDTH BITS_PER_LONG
+#define ACPI_MACHINE_WIDTH          BITS_PER_LONG
 #define ACPI_USE_NATIVE_MATH64
-#define ACPI_EXPORT_SYMBOL(symbol) EXPORT_SYMBOL(symbol);
-#define strtoul simple_strtoul
+#define ACPI_EXPORT_SYMBOL(symbol)  EXPORT_SYMBOL(symbol);
+#define strtoul                     simple_strtoul
 
-#define ACPI_CACHE_T struct kmem_cache
-#define ACPI_SPINLOCK spinlock_t *
-#define ACPI_CPU_FLAGS unsigned long
+#define ACPI_CACHE_T                struct kmem_cache
+#define ACPI_SPINLOCK               spinlock_t *
+#define ACPI_CPU_FLAGS              unsigned long
 
-#define ACPI_UINTPTR_T uintptr_t
+#define ACPI_UINTPTR_T              uintptr_t
 
-#define ACPI_TO_INTEGER(p) ((uintptr_t)(p))
-#define ACPI_OFFSET(d, f) offsetof(d, f)
+#define ACPI_TO_INTEGER(p)          ((uintptr_t)(p))
+#define ACPI_OFFSET(d, f)           offsetof(d, f)
 
 /*
  * Overrides for in-kernel ACPICA
@@ -151,17 +162,61 @@
 #define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsGetNextFilename
 #define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsCloseDirectory
 
-#define ACPI_MSG_ERROR KERN_ERR ""
-#define ACPI_MSG_EXCEPTION KERN_ERR "@FATAL"
-#define ACPI_MSG_WARNING KERN_WARNING ""
-#define ACPI_MSG_INFO KERN_INFO ""
+#define ACPI_MSG_ERROR          KERN_ERR "ACPI Error: "
+#define ACPI_MSG_EXCEPTION      KERN_ERR "ACPI Exception: "
+#define ACPI_MSG_WARNING        KERN_WARNING "ACPI Warning: "
+#define ACPI_MSG_INFO           KERN_INFO "ACPI: "
 
-#define ACPI_MSG_BIOS_ERROR KERN_ERR "ACPI BIOS Error (bug): "
-#define ACPI_MSG_BIOS_WARNING KERN_WARNING "ACPI BIOS Warning (bug): "
+#define ACPI_MSG_BIOS_ERROR     KERN_ERR "ACPI BIOS Error (bug): "
+#define ACPI_MSG_BIOS_WARNING   KERN_WARNING "ACPI BIOS Warning (bug): "
 
 /*
  * Linux wants to use designated initializers for function pointer structs.
  */
-#define ACPI_STRUCT_INIT(field, value) .field = value
+#define ACPI_STRUCT_INIT(field, value)  .field = value
+
+#else /* !__KERNEL__ */
+
+/* Define/disable kernel-specific declarators */
+
+#ifndef __init
+#define __init
+#endif
+#ifndef __iomem
+#define __iomem
+#endif
+
+/* Host-dependent types and defines for user-space ACPICA */
+
+#define ACPI_FLUSH_CPU_CACHE()
+#define ACPI_CAST_PTHREAD_T(Pthread) ((ACPI_THREAD_ID) (Pthread))
+
+#if defined(__ia64__)    || (defined(__x86_64__) && !defined(__ILP32__)) ||\
+    defined(__aarch64__) || defined(__PPC64__) ||\
+    defined(__s390x__)   || defined(__loongarch__) ||\
+    (defined(__riscv) && (defined(__LP64__) || defined(_LP64)))
+#define ACPI_MACHINE_WIDTH          64
+#define COMPILER_DEPENDENT_INT64    long
+#define COMPILER_DEPENDENT_UINT64   unsigned long
+#else
+#define ACPI_MACHINE_WIDTH          32
+#define COMPILER_DEPENDENT_INT64    long long
+#define COMPILER_DEPENDENT_UINT64   unsigned long long
+#define ACPI_USE_NATIVE_DIVIDE
+#define ACPI_USE_NATIVE_MATH64
+#endif
+
+#ifndef __cdecl
+#define __cdecl
+#endif
+
+
+/* Rust-specific changes */
+
+#define ACPI_CACHE_T                ACPI_MEMORY_LIST
+#define ACPI_USE_LOCAL_CACHE        1
+
+
+#endif /* __KERNEL__ */
 
 #endif /* __ACRUST_H__ */
